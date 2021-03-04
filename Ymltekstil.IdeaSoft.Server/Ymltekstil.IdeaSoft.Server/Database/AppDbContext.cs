6b9e9b4c411e0a4f4b12af8ce2979718b612a0d7
ibrahim <ibrahimgazaloglu@gmail.com>
2021-03-04 08:59:46 +0100

    Add refresh token migration

--- Ymltekstil.IdeaSoft.Server/Ymltekstil.IdeaSoft.Server/Database/AppDbContext.cs
+++ Ymltekstil.IdeaSoft.Server/Ymltekstil.IdeaSoft.Server/Database/AppDbContext.cs
@@ -11,14 +11,6 @@ namespace Ymltekstil.IdeaSoft.Server.Database
 
             optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=yml;Integrated Security=True");
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            string refreshToken = "NzdiZDlmYzNhYzc2ZGU0ZDA1YzQ1YjRlNjAzNDRlOTI3OTNhZjViODY3ZGZkYTFhZDFlOGQ1NzJmMmE5NmY5Yw";
-
-            modelBuilder.Entity<Settings>().HasData(new Settings { Key = "RefreshToken", Value = refreshToken, Id = Guid.NewGuid() });
-        }
-
         public DbSet<Settings> Settings { get; set; }
 
     }
---
+++ Ymltekstil.IdeaSoft.Server/Ymltekstil.IdeaSoft.Server/Migrations/20210304065400_RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ymltekstil.IdeaSoft.Server.Migrations
+{
+    public partial class RefreshToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Settings SET Value = 'YTMzNzg5NDllOTE2MjUzMmJlMWQyZjQwZDM4NDhkMDM1NDI5ODk1MGFiYmM5YzYzMzNkMzMyYzYwZTY4OWQ0Mg' WHERE \"Key\" = 'RefreshToken'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+                  }
+    }
+}
